93baa144f4cc378b816109d9fe8b6e37042afa97
Yusuf Atakan Özmen <92385960+yusufatakanozmenn@users.noreply.github.com>
2024-05-29 18:56:03 +0300

    admin header-sidebar added

---
+++ admin/include/sidebar.php
@@ -0,0 +1,24 @@
+<div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
+    <div class="brand-logo">
+        <a href="index.html">
+            <img src="./assets/images/logo-icon.png" class="logo-icon" alt="logo icon" />
+            <h5 class="logo-text">Dashtreme Admin</h5>
+        </a>
+    </div>
+    <ul class="sidebar-menu do-nicescrol">
+        <li class="sidebar-header">MAIN NAVIGATION</li>
+        <li>
+            <a href="create_job.php">
+                <i class="zmdi zmdi-assignment"></i> <span>Create Jobs</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="profile.php">
+                <i class="zmdi zmdi-account"></i> <span>Profile</span>
+            </a>
+        </li>
+    </ul>
+
+
+</div>
